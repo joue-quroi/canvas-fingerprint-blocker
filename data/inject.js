3cd03dc9234a842245270f020a3ba4dab6b0b718
Ganlv <ganlvtech@qq.com>
2019-04-09 03:21:27 +0800

    replace code string with function

--- data/inject.js
+++ data/inject.js
@@ -6,54 +6,55 @@
     return;
   }
 
-  var script = document.createElement('script');
-  script.textContent = `
-{
-  const toBlob = HTMLCanvasElement.prototype.toBlob;
-  const toDataURL = HTMLCanvasElement.prototype.toDataURL;
+  var inject = function () {
+    const toBlob = HTMLCanvasElement.prototype.toBlob;
+    const toDataURL = HTMLCanvasElement.prototype.toDataURL;
 
-  HTMLCanvasElement.prototype.htGfd = function() {
-    const {width, height} = this;
-    const context = this.getContext('2d');
-    const shift = {
-      'r': Math.floor(Math.random() * 10) - 5,
-      'g': Math.floor(Math.random() * 10) - 5,
-      'b': Math.floor(Math.random() * 10) - 5
-    };
-    const matt = context.getImageData(0, 0, width, height);
-    for (let i = 0; i < height; i += 3) {
-      for (let j = 0; j < width; j += 3) {
-        const n = ((i * (width * 4)) + (j * 4));
-        matt.data[n + 0] = matt.data[n + 0] + shift.r;
-        matt.data[n + 1] = matt.data[n + 1] + shift.g;
-        matt.data[n + 2] = matt.data[n + 2] + shift.b;
+    HTMLCanvasElement.prototype.htGfd = function() {
+      const {width, height} = this;
+      const context = this.getContext('2d');
+      const shift = {
+        'r': Math.floor(Math.random() * 10) - 5,
+        'g': Math.floor(Math.random() * 10) - 5,
+        'b': Math.floor(Math.random() * 10) - 5
+      };
+      const matt = context.getImageData(0, 0, width, height);
+      for (let i = 0; i < height; i += 3) {
+        for (let j = 0; j < width; j += 3) {
+          const n = ((i * (width * 4)) + (j * 4));
+          matt.data[n + 0] = matt.data[n + 0] + shift.r;
+          matt.data[n + 1] = matt.data[n + 1] + shift.g;
+          matt.data[n + 2] = matt.data[n + 2] + shift.b;
+        }
       }
-    }
-    context.putImageData(matt, 0, 0);
-    this.htGfd = () => {
+      context.putImageData(matt, 0, 0);
+      this.htGfd = () => {
+        window.top.postMessage('htGfd-called', '*');
+      };
       window.top.postMessage('htGfd-called', '*');
     };
-    window.top.postMessage('htGfd-called', '*');
-  };
 
-  Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
-    value: function() {
-      if (document.documentElement.dataset.htgfd !== 'false') {
-        this.htGfd();
+    Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+      value: function() {
+        if (document.documentElement.dataset.htgfd !== 'false') {
+          this.htGfd();
+        }
+        return toBlob.apply(this, arguments);
       }
-      return toBlob.apply(this, arguments);
-    }
-  });
-  Object.defineProperty(HTMLCanvasElement.prototype, 'toDataURL', {
-    value: function() {
-      if (document.documentElement.dataset.htgfd !== 'false') {
-        this.htGfd();
+    });
+    Object.defineProperty(HTMLCanvasElement.prototype, 'toDataURL', {
+      value: function() {
+        if (document.documentElement.dataset.htgfd !== 'false') {
+          this.htGfd();
+        }
+        return toDataURL.apply(this, arguments);
       }
-      return toDataURL.apply(this, arguments);
-    }
-  });
-  document.documentElement.dataset.htGfd = true;
-}`;
+    });
+    document.documentElement.dataset.htGfd = true;
+  }
+
+  var script = document.createElement('script');
+  script.textContent = '(' + inject + ')();';
   document.documentElement.appendChild(script);
   // make sure the script is injected
   if (document.documentElement.dataset.htGfd !== 'true') {
